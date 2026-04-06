5d31dad187b56a65a6066d6320603fd06b846385
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-04-06 13:51:02 +0600

    u

--- src/app/utils/multer.ts
+++ src/app/utils/multer.ts
@@ -1,9 +1,12 @@
 import multer from 'multer';
 import path from 'path';
+import os from 'os';
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.resolve(process.cwd(), 'uploads'));
+    // Write directly to the operating system's temp directory (/tmp)
+    // as it is the only writable directory on Vercel Serverless Functions
+    cb(null, os.tmpdir());
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
